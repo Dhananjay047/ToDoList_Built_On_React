905c69c434532924007cb9c3f83c50040f63e726
Dhananjay047 <72164419+Dhananjay047@users.noreply.github.com>
2021-04-06 16:29:31 +0530

    Update and rename Ul.jsx to ToDoItems.jsx

---
+++ src/components/ToDoItems.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function ToDoItem(props) {
+  const [status, change] = React.useState(false);
+
+  function changeState() {
+    if (status) {
+      change(false);
+    } else {
+      change(true);
+    }
+  }
+  return (
+    <div
+      onDoubleClick={() => {
+        props.onChecked(props.id);
+      }}
+      onClick={changeState}
+      style={{ textDecoration: status ? "line-through" : "none" }}
+    >
+      <li>{props.text}</li>
+    </div>
+  );
+}
+
+export default ToDoItem;
